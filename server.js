8a57beb90a6f6de240a0b322f4ee0f85f4365faf
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-29 09:43:33 +0100

    Install request, make post request to api endpoint in local get

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ let express = require('express')
 let app = express()
 let port = process.env.PORT || 8080
 let bodyParser = require('body-parser')
+let request = require('request')
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -13,6 +14,9 @@ app.listen(port)
 console.log("You're on LH " + port)
 
 app.get('/', function(req, res){
+  request.post('http://data.fixer.io/api/latest?access_key=e4aaa0f54f499ab5f7f99b2929173560&symbols=gbp', function(err, res, body) {
+    console.log(body)
+  })
   res.render('index')
 })
 
