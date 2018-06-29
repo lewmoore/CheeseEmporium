f1dfd63b32e350e2108aed14b7ce7e18e99e6e46
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-29 11:08:17 +0100

    Add initial cheesecalculation with mock api test

--- server.js
+++ server.js
@@ -16,9 +16,14 @@ console.log("You're on LH " + port)
 
 app.get('/', function(req, res){
   request.post('http://data.fixer.io/api/latest?access_key=' + apikey + '&symbols=gbp', function(err, response, body) {
+
     let parsedData = JSON.parse(response.body)
-    console.log(parsedData.rates)
-    res.render('index', {parsedData: parsedData})
+    let dataRes = parsedData.rates.GBP
+    let conversionRate = 1 / dataRes
+    let customerEUR = 19.99 * conversionRate
+    let cheeseAmount = customerEUR / 3.24
+
+    res.render('index', {cheeseAmount: cheeseAmount})
   })
 })
 
--- test/server.test.js
+++ test/server.test.js
@@ -3,10 +3,13 @@ let chaiHttp = require('chai-http')
 let server = require('../server')
 let expect = chai.expect
 let should = chai.should()
+let nock = require('nock')
+let apikey = require('../apikey')
 
 chai.use(chaiHttp)
 
 describe('landing page', function(){
+
   it('should not error', function(done){
     chai.request(server)
     .get('/')
@@ -15,5 +18,19 @@ describe('landing page', function(){
       res.text.should.contain('Welcome to the Cheese Emporium!')
     })
     done()
+  })
+
+  it('should make a post request to API with correct response', function(){
+    let apiMock = nock('http://data.fixer.io')
+                  .post('/api/latest?access_key=' + apikey + '&symbols=gbp')
+                  .reply(200, { gbp: '0.767' })
+
+    chai.request(server)
+    .get('/')
+    .end(function(req, res){
+      res.should.have.status(200)
+      expect(res.body).toEqual({ gbp: '0.767' })
+      console.log(res.body, "RES TEXT")
+    })
   })
 })
